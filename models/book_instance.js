d1995b642322389525829ff8b54093379f7bf228
lutkenhaus <gustavomayron9@gmail.com>
2023-10-29 13:37:16 -0300

    initial commit

---
+++ models/book_instance.js
@@ -0,0 +1,34 @@
+var mongoose = require("mongoose");
+const { DateTime } = require("luxon");
+
+var Schema = mongoose.Schema;
+
+var BookInstanceSchema = new Schema({
+  book: { type: Schema.Types.ObjectId, ref: "Book", required: true }, //reference to the associated book
+  imprint: { type: String, required: true },
+  status: {
+    type: String,
+    required: true,
+    enum: ["Available", "Maintenance", "Loaned", "Reserved"],
+    default: "Maintenance",
+  },
+  due_back: { type: Date, default: Date.now },
+});
+
+// Virtual for bookinstance's URL
+BookInstanceSchema.virtual("url").get(function () {
+  return "/catalog/book_instance/" + this._id;
+});
+
+// Virtual for bookinstance's DateTime formatted due back date
+BookInstanceSchema.virtual("due_back_formatted").get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+});
+
+// Virtual for bookinstance's due back yyyy-mm-dd format
+BookInstanceSchema.virtual("due_back_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.due_back).toISODate(); // format 'YYYY-MM-DD'
+});
+
+//Export model
+module.exports = mongoose.model("BookInstance", BookInstanceSchema);
